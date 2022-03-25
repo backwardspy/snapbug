64c63b2ef6fa23b1cf5f7b3df15b43ec77373f4a
backwardspy <backwardspy@gmail.com>
2022-03-25 19:48:40 +0000

    perf: :zap: function search optimisation
    saves scanned code to a single file for efficient searching later on

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,17 @@
 use std::{
     collections::{HashMap, HashSet},
     fs::File,
-    io::{BufRead, BufReader},
+    io::{BufRead, BufReader, BufWriter, Seek, SeekFrom, Write},
     path::{Path, PathBuf},
 };
 
 use anyhow::{anyhow, Result};
 use clap::Parser;
 use regex::Regex;
+use tempfile::tempfile;
 use walkdir::{DirEntry, WalkDir};
 
+/// Find potentially unused functions in a python source tree.
 #[derive(Parser)]
 #[clap(author, version, about)]
 struct Args {
@@ -53,31 +55,9 @@ fn is_python_file(entry: &DirEntry) -> bool {
             .unwrap_or(false)
 }
 
-/// Checks a file for captures with the given regex and returns them all.
-fn get_captures<'a>(
-    entry: &'a DirEntry,
-    pattern: &'a Regex,
-) -> Result<impl Iterator<Item = (usize, String)> + 'a> {
-    Ok(BufReader::new(File::open(entry.path())?)
-        .lines()
-        .enumerate()
-        .filter_map(|(lineno, r)| match r {
-            Ok(r) => Some((lineno + 1, r)),
-            _ => None,
-        })
-        .map(|(lineno, line)| {
-            pattern
-                .captures(&line)?
-                .get(1)
-                .map(|m| (lineno, m.as_str().to_owned()))
-        })
-        .flatten()
-        .into_iter())
-}
-
 /// Returns an iterator over all python files in the given path.
 fn make_walker(path: &Path) -> Result<impl Iterator<Item = DirEntry>> {
-    Ok(WalkDir::new(path.canonicalize()?)
+    Ok(WalkDir::new(path)
         .into_iter()
         .filter_entry(is_useful)
         .filter_map(|e| e.ok())
@@ -89,61 +69,85 @@ fn should_consider_function(name: &String) -> bool {
     !name.contains("test_") && !name.contains("__")
 }
 
-fn counts_to_functions(counts: HashMap<&Function, usize>) -> Vec<&Function> {
+/// Return all functions that are only mentioned once.
+fn find_unused_functions(counts: HashMap<&Function, usize>) -> Vec<&Function> {
     let mut unused_functions = counts
         .into_iter()
         .filter(|(_, count)| *count < 2)
         .map(|k| k.0)
         .collect::<Vec<&Function>>();
     unused_functions.sort_by_key(|function| &function.location);
+
     unused_functions
 }
 
-fn main() -> Result<()> {
-    let args = Args::parse();
-
+/// Walk the given path, finding all declared functions.
+/// Also populates the haystack file used later for counting references.
+fn scan_path(path: &Path, haystack_file: &File) -> Result<HashSet<Function>> {
     let function_pattern = Regex::new(r"^[^#]*def (\S.*)\s*\(.*$")?;
     let mut functions = HashSet::new();
 
-    let walker = make_walker(&args.path)?;
+    let mut haystack_writer = BufWriter::new(haystack_file);
+
+    let walker = make_walker(path)?;
     for entry in walker {
-        functions.extend(
-            get_captures(&entry, &function_pattern)?
-                .filter(|(_, name)| should_consider_function(name))
-                .map(|(lineno, s)| Function {
-                    name: s,
-                    location: (entry.path().to_owned(), lineno),
-                }),
-        );
+        for (lineno, line) in BufReader::new(File::open(entry.path())?)
+            .lines()
+            .enumerate()
+        {
+            let line = line?;
+            if let Some(name) = function_pattern
+                .captures(&line)
+                .map(|c| c.get(1).unwrap().as_str().to_owned())
+            {
+                if should_consider_function(&name) {
+                    let location = (entry.path().to_owned(), lineno + 1);
+                    functions.insert(Function { name, location });
+                }
+            }
+            haystack_writer.write_all(line.as_bytes())?;
+        }
     }
 
+    Ok(functions)
+}
+
+/// Scan the haystack file to find functions that are only mentioned once.
+fn scan_for_unused_functions<'a>(
+    haystack: &File,
+    functions: &'a HashSet<Function>,
+) -> Result<Vec<&'a Function>> {
     let mut counts = HashMap::new();
 
-    let walker = make_walker(&args.path)?;
-    for entry in walker {
-        BufReader::new(File::open(entry.path())?)
-            .lines()
-            .filter_map(|r| r.ok())
-            .for_each(|line| {
-                functions
-                    .iter()
-                    .map(|function| (function, line.matches(&function.name).count()))
-                    .for_each(|(function, count)| *counts.entry(function).or_insert(0) += count);
-            })
+    for line in BufReader::new(haystack).lines() {
+        let line = line?;
+        for function in functions {
+            *counts.entry(function).or_insert(0) += line.matches(&function.name).count();
+        }
     }
 
-    let unused_functions = counts_to_functions(counts);
+    Ok(find_unused_functions(counts))
+}
+
+/// Find potentially unused functions in a python source tree.
+fn main() -> Result<()> {
+    let args = Args::parse();
+    let root = args.path.canonicalize()?;
+
+    let mut haystack = tempfile()?;
+    let functions = scan_path(&root, &haystack)?;
+
+    haystack.seek(SeekFrom::Start(0))?;
+    let unused_functions = scan_for_unused_functions(&haystack, &functions)?;
+
     let should_fail = unused_functions.len() > 0;
 
-    let root = args.path.canonicalize()?;
     for function in unused_functions {
         eprintln!(
             "{}:{} - function \"{}\" may be unused",
-            function
-                .location
-                .0
-                .strip_prefix(&root)
-                .map(|path| path.to_string_lossy())?,
+            args.path
+                .join(function.location.0.strip_prefix(&root)?)
+                .display(),
             function.location.1,
             function.name
         )
